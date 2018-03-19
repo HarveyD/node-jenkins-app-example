d62596ee97d848bb83f9b4406b4619683fe520b6
Harvey <harveydelaney@yahoo.com.au>
2018-03-19 20:41:27 +1100

    initial commit

---
+++ spec/helloWorld-spec.js
@@ -0,0 +1,21 @@
+var request = require("request");
+var helloWorld = require("../app.js");
+var base_url = "http://localhost:3000/";
+
+describe("Hello World Server", function() {
+  describe("GET /", function() {
+    it("returns status code 200", function(done) {
+      request.get(base_url, function(error, response, body) {
+        expect(response.statusCode).toBe(200);
+        done();
+      });
+    });
+
+    it("returns Hello World", function(done) {
+      request.get(base_url, function(error, response, body) {
+        expect(body).toBe("Hello World!");
+        done();
+      });
+    });
+  });
+});
