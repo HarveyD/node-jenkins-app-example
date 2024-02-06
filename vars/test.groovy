e8cf632b0dfadbf3053259498ac04641228d132b
Jyothi-03 <129101057+Jyothi-03@users.noreply.github.com>
2024-02-06 23:13:20 +0530

    Update test.groovy

--- vars/test.groovy
+++ vars/test.groovy
@@ -1,4 +1,4 @@
 def call() {
     // Run test script or command
-    sh 'npm test'
+    sh 'var/lib/docker/volumes/jenkins_home/_data/tools/jenkins.plugins.nodejs.tools.NodeJSInstallation/Node-Build/bin/npm test'
 }
