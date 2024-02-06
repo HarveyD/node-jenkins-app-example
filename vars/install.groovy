48826f2b910a7b032328363f3922a6dcd284e37f
Jyothi-03 <129101057+Jyothi-03@users.noreply.github.com>
2024-02-06 21:56:08 +0530

    Update install.groovy

--- vars/install.groovy
+++ vars/install.groovy
@@ -1,59 +1,25 @@
 
-// def prerequisite() {
-    
-//         // Run npm install
-//         sh 'npm install'
-    
-// }
-
-// def build() {
-    
-//         // Run npm start
-//         sh 'npm start &'
-    
-// }
-
-// def test() {
-    
-//         // Run npm test
-//         sh 'npm test'
-    
-// }
-
-// return this
-// }
-
-
-// Define a function to execute npm commands
-def executeNpmCommand(String command) {
-    // Check if the 'npm' command exists in PATH
-    def npmCommandExists = sh(returnStdout: true, script: 'command -v npm').trim()
-
-    if (!npmCommandExists) {
-        error('npm command not found. Please ensure npm is installed and available in the PATH environment variable.')
-    }
-
-    // Execute the npm command
-    sh "npm ${command}"
-}
-
-// Define a function to install prerequisites
 def prerequisite() {
-    // Run npm install
-    executeNpmCommand('install')
+    
+        // Run npm install
+        sh 'npm install'
+    
 }
 
-// Define a function to build
 def build() {
-    // Run npm start
-    executeNpmCommand('start &')
+    
+        // Run npm start
+        sh 'npm start &'
+    
 }
 
-// Define a function to run tests
 def test() {
-    // Run npm test
-    executeNpmCommand('test')
+    
+        // Run npm test
+        sh 'npm test'
+    
 }
 
-// Return the shared code object
+return this
+
 return this
