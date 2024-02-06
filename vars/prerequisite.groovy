439027f60cfbd1943bb65526f1a1ae77fde41f36
Jyothi-03 <129101057+Jyothi-03@users.noreply.github.com>
2024-02-06 23:13:09 +0530

    Update prerequisite.groovy

--- vars/prerequisite.groovy
+++ vars/prerequisite.groovy
@@ -1,4 +1,4 @@
 def call() {
     // Run npm install
-    sh 'npm install'
+    sh 'var/lib/docker/volumes/jenkins_home/_data/tools/jenkins.plugins.nodejs.tools.NodeJSInstallation/Node-Build/bin/npm install'
 }
