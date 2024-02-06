862bee0c71c63c7eecb38563b7a6c1156f5cdc0f
Jyothi-03 <129101057+Jyothi-03@users.noreply.github.com>
2024-02-06 21:14:38 +0530

    Update install.groovy

--- vars/install.groovy
+++ vars/install.groovy
@@ -1,9 +1,4 @@
-def call(body) {
-    def config = [:]
-    body.resolveStrategy = Closure.DELEGATE_FIRST
-    body.delegate = config
-    body()
-    
+
 def prerequisite() {
     
         // Run npm install
