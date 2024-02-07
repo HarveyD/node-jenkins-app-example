20f30e017db99f72737059d9cfcfd96e0a0b00d0
Jyothi-03 <129101057+Jyothi-03@users.noreply.github.com>
2024-02-07 11:25:44 +0530

    Update install.groovy

--- vars/install.groovy
+++ vars/install.groovy
@@ -1,1 +1,14 @@
-def 
+def call() {
+    // Run build script or command
+    sh 'npm run build'
+}
+
+def call() {
+    // Run npm install
+    sh 'npm install'
+}
+
+def call() {
+    // Run test script or command
+    sh 'npm test'
+}
