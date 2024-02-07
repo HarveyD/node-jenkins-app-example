f9d16a949d25b45ff0e539f4f15d4cbc48aa42fb
Jyothi-03 <129101057+Jyothi-03@users.noreply.github.com>
2024-02-07 11:39:35 +0530

    Update install.groovy

--- vars/install.groovy
+++ vars/install.groovy
@@ -1,14 +1,24 @@
-def call() {
-    // Run build script or command
-    sh 'npm run build'
-}
+// def call() {
+//     // Run build script or command
+//     sh 'npm run build'
+// }
+
+// def call() {
+//     // Run npm install
+//     sh 'npm install'
+// }
+
+// def call() {
+//     // Run test script or command
+//     sh 'npm test'
+// }
+
+
 
 def call() {
-    // Run npm install
+    // Define the commands to install dependencies, build, and test your Node.js application
     sh 'npm install'
-}
-
-def call() {
-    // Run test script or command
+    sh 'npm run build'
     sh 'npm test'
 }
+
