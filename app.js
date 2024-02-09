936a6ae5af682b86163742c7655c66c609597e0d
Jyothi-03 <129101057+Jyothi-03@users.noreply.github.com>
2024-02-09 16:23:43 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 
-app.get('/', (req, res) => res.send('Hii World, How r u?'))
+app.get('/', (req, res) => res.send('Hello World'))
 
 var server = app.listen(3000, () => {
     console.log("Listening on port " + server.address().port + "...");
