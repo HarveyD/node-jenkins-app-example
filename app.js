0dd9fed44f871f02e9ed59e19d51438253fb5d90
Jyothi-03 <129101057+Jyothi-03@users.noreply.github.com>
2024-02-09 09:14:48 +0530

    Update app.js

--- app.js
+++ app.js
@@ -13,7 +13,7 @@
 var express = require('express');
 var app = express();
 app.get('/', function (req, res) {
-  res.send('Hello Jyothi!');
+  res.send('Hello Jyothi!, I am an IBM intern');
 });
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
